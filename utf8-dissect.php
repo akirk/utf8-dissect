672cdb3491a7f9dd049d9a63343cb8ec7e09251c
Alexander Kirk <alexander@kirk.at>
2015-08-27 10:30:27 +0200

    refactor into a class

--- utf8-dissect.php
+++ utf8-dissect.php
@@ -3,23 +3,44 @@
 // Unknown license
 // with some enhancements by Alex Kirk in 2015
 
-class UTF8 {
-	private static $names;
+class UTF8_Dissect {
+	private static $nameslist;
+	public static $names = '', $result = '', $entities = '';
+	const DOWNLOAD_TO_TEMP = true;
 
 	private static function loadNames() {
-		if ( !self::$names ) {
-			self::$names = file_get_contents( __DIR__ . '/NamesList.txt' );
+		if ( !self::$nameslist ) {
+			if ( self::DOWNLOAD_TO_TEMP ) {
+				$filename = sys_get_temp_dir() . '/NamesList.txt';
+			} else {
+				$filename = __DIR__ . '/NamesList.txt';
+			}
+
+			if ( ! file_exists( $filename ) ) {
+				$nameslist_txt = 'http://www.unicode.org/Public/UNIDATA/NamesList.txt';
+
+				if ( self::DOWNLOAD_TO_TEMP ) {
+					echo 'Downloading NamesList.txt...<br/>';
+					file_put_contents( $filename, file_get_contents( $nameslist_txt ) );
+
+					if ( ! file_exists( $filename ) || ! filesize( $filename) ) {
+						echo 'Could not download NamesList.txt!';
+						exit;
+					}
+				} else {
+					echo 'Please download NamesList.txt from ', $nameslist_txt;
+					exit;
+				}
+			}
+			self::$nameslist = file_get_contents( $filename );
 		}
 	}
 
 	public static function getName( $code ) {
-		$result = '';
-		$names = '';
-
 		self::loadNames();
 
 		$separator = "\n";
-		$data = strtok( self::$names, $separator );
+		$data = strtok( self::$nameslist, $separator );
 
 		while ($data !== false) {
 			if ( preg_match( "#^$code\t#", $data ) ) {
@@ -33,105 +54,105 @@ class UTF8 {
 		if ( ! $data) {
 			return false;
 		}
-		$result .= 'U+' . $code;
+		self::$result .= 'U+' . $code;
 		$charname = trim( $data );
 		$charitself = hexdec($code) < 0x7F ?
 			(hexdec($code) <= 0x20 || (hexdec($code) >= 0x61 && hexdec($code) <= 0x7A)
 			|| (hexdec($code) >= 0x41 && hexdec($code) <= 0x5A)) ? '' : ' (' . chr(hexdec($code)) . ')' : " (&#x$code;)";
-		$names .= "U+$charname character$charitself\n";
+		self::$names .= "U+$charname character$charitself\n";
 
 		while ( $data !== false && substr( $data, 0, 1 ) === "\t" ) {
-			$result .= $data;
+			self::$result .= $data;
 			$data = strtok( $separator );
 		}
 
-		return array( $result, $names );
+	}
+
+	public static function dissect( $query ) {
+
+		$bytes = unpack( 'C*', $query );
+		if ( ! $bytes ) {
+			return false;
+		}
+
+		self::$result = '';
+		self::$entities = '';
+		self::$names = '';
+		$remaining = 0;
+		$count = 0;
+		$scratch = 0;
+		$index = 0;
+
+		foreach ( $bytes as $raw ) {
+			++$index;
+
+			self::$result .= sprintf("\nByte number $index is decimal %d, hex 0x%02X, octal %04o, binary %08b\n", $raw, $raw, $raw, $raw);
+			if ($raw == 0xFE or $raw == 0xFF) { // UTF-8 validity test
+				echo "Not a valid UTF-8 byte.\n";
+				return false;
+			}
+			if (($raw & 0b11000000) == 0b10000000) {
+				// continuation byte
+				// check we're expecting one
+				if (! $remaining) {
+					echo "Unexpected continuation byte.\n";
+					return false;
+				}
+				$r = $remaining - 1;
+				self::$result .= "This is continuation byte $count, expecting $r more.\n";
+				// strip off the high bit
+				$raw &=~ 0b10000000;
+			} else {
+				// check we're not expecting more continuation bytes
+				if ($remaining) {
+					self::$result .= "Previous UTF-8 multibyte sequence incomplete, earlier bytes dropped.\n";
+				}
+				// count how many leading bits are on
+				$bit = 7;
+				$remaining = 0;
+				$count = 0;
+				$scratch = 0;
+				while (($bit >= 0) and ($raw & (1 << $bit)) > 0) {
+					// one more byte expected
+					++$remaining;
+					// turn off the bit
+					$raw &=~ (1 << $bit);
+					// ready for next bit
+					--$bit;
+				}
+				// $remaining must be 0, 2, 3, 4, 5, or 6
+				if ( $remaining == 0 ) $remaining = 1;
+				if ($remaining < 1 or $remaining > 6) {
+					echo "Not a valid UTF-8 byte (internal error during decoding: remaining == $remaining).\n";
+					return false;
+				}
+				if ($remaining > 1) {
+					self::$result .= "This is the first byte of a $remaining byte sequence.\n";
+				}
+			}
+			// add the current byte to the pending number
+			--$remaining;
+
+			++$count;
+			$scratch += $raw << (6 * $remaining);
+			if (!$remaining) {
+				self::$entities .= '&#x' . sprintf('%04x', $scratch) . ';';
+				$scratch = sprintf('%04X', $scratch);
+				self::$result .= "\n";
+				self::getName( $scratch );
+				self::$result .= "\n";
+			}
+		};
+
+		if ($remaining) {
+			self::$result .= "End of file during multibyte sequence, some bytes dropped\n";
+		}
+
+		return true;
 	}
 }
 
-$names = $query = false;
-if (!empty( $_GET['query'] ) ) {
-
-	$query = $_GET['query'];
-	$bytes = unpack( 'C*', $query );
-
-	$result = '';
-	$entities = '';
-	$names = '';
-	$remaining = 0;
-	$count = 0;
-	$scratch = 0;
-	$index = 0;
-
-	foreach ( $bytes as $raw ) {
-		++$index;
-
-		$result .= sprintf("\nByte number $index is decimal %d, hex 0x%02X, octal %04o, binary %08b\n", $raw, $raw, $raw, $raw);
-		if ($raw == 0xFE or $raw == 0xFF) { // UTF-8 validity test
-			echo "Not a valid UTF-8 byte.\n";
-			exit;
-		}
-		if (($raw & 0b11000000) == 0b10000000) {
-			// continuation byte
-			// check we're expecting one
-			if (! $remaining) {
-				echo "Unexpected continuation byte.\n";
-				exit;
-			}
-			$r = $remaining - 1;
-			$result .= "This is continuation byte $count, expecting $r more.\n";
-			// strip off the high bit
-			$raw &=~ 0b10000000;
-		} else {
-			// check we're not expecting more continuation bytes
-			if ($remaining) {
-				$result .= "Previous UTF-8 multibyte sequence incomplete, earlier bytes dropped.\n";
-			}
-			// count how many leading bits are on
-			$bit = 7;
-			$remaining = 0;
-			$count = 0;
-			$scratch = 0;
-			while (($bit >= 0) and ($raw & (1 << $bit)) > 0) {
-				// one more byte expected
-				++$remaining;
-				// turn off the bit
-				$raw &=~ (1 << $bit);
-				// ready for next bit
-				--$bit;
-			}
-			// $remaining must be 0, 2, 3, 4, 5, or 6
-			if ( $remaining == 0 ) $remaining = 1;
-			if ($remaining < 1 or $remaining > 6) {
-				echo "Not a valid UTF-8 byte (internal error during decoding: remaining == $remaining).\n";
-				exit;
-			}
-			if ($remaining > 1) {
-				$result .= "This is the first byte of a $remaining byte sequence.\n";
-			}
-		}
-		// add the current byte to the pending number
-		--$remaining;
-
-		++$count;
-		$scratch += $raw << (6 * $remaining);
-		if (!$remaining) {
-			$entities .= '&#x' . sprintf('%04x', $scratch) . ';';
-			$scratch = sprintf('%04X', $scratch);
-			$result .= "\n";
-			if ( $data = UTF8::getName( $scratch ) ) {
-				$result .= $data[0];
-				$names .= $data[1];
-			}
-			$result .= "\n";
-		}
-	};
-
-	if ($remaining) {
-		$result .= "End of file during multibyte sequence, some bytes dropped\n";
-	}
-
-}
+$query = empty( $_GET['query'] ) ? false : mb_substr( $_GET['query'], 0, 255 );
 
 header( 'Content-Type: text/html;charset=utf-8' );
 
@@ -151,15 +172,16 @@ header( 'Content-Type: text/html;charset=utf-8' );
 		Enter your UTF8 text: <input type="text" name="query" value="<?php echo htmlentities( $query ); ?>" />
 		<button>Submit</button>
 	</form>
-	<?php if ($names): ?>
+	<?php if ( UTF8_Dissect::dissect( $query ) ): ?>
+		<h2>Dissection result</h2>
 		<p>As character names:</p>
-		<pre><?php echo htmlentities( $names ); ?></pre>
+		<pre><?php echo htmlentities( UTF8_Dissect::$names ); ?></pre>
 		<p>As raw characters:</p>
-		<pre><?php echo $entities; ?></pre>
+		<pre><?php echo UTF8_Dissect::$entities; ?></pre>
 		<p>As a string of HTML entities:</p>
-		<pre><?php echo htmlentities( $entities ); ?></pre>
+		<pre><?php echo htmlentities( UTF8_Dissect::$entities ); ?></pre>
 		<p>Decoder output:</p>
-		<pre><?php echo htmlentities( $result ); ?></pre>
+		<pre><?php echo htmlentities( UTF8_Dissect::$result ); ?></pre>
 	<?php endif; ?>
 </body>
 </html>
